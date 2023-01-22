ba1a6e8b0c8ddd9edd0ae9ea06ba39d4a4801302
systemdev <admin@riven-dell.ru>
2023-01-22 12:20:36 +0500

    Fixed exceeding the limit of sack

--- src/main/java/org/systemdev/mc/rdsacks/Events/PickupEvent.java
+++ src/main/java/org/systemdev/mc/rdsacks/Events/PickupEvent.java
@@ -4,7 +4,6 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
@@ -12,8 +11,6 @@ import org.bukkit.persistence.PersistentDataType;
 import org.systemdev.mc.rdsacks.Config.Configuration;
 import org.systemdev.mc.rdsacks.RDSacks;
 import org.systemdev.mc.rdsacks.Utilities;
-
-import java.util.EventListener;
 
 public class PickupEvent implements Listener {
 
@@ -28,12 +25,23 @@ public class PickupEvent implements Listener {
         Integer maxAmount = container.get(Utilities.maxItems, PersistentDataType.INTEGER);
         NamespacedKey currentKey = new NamespacedKey(RDSacks.getInstance(), String.valueOf(e.getItem().getItemStack().getType()));
         Integer currentAmount = container.get(currentKey, PersistentDataType.INTEGER);
-        RDSacks.getInstance().log.info(sackType);
-        if(Utilities.findStringInList(Configuration.GetSackPickupMaterials(sackType), String.valueOf(e.getItem().getItemStack().getType())) && currentAmount < maxAmount) {
-            Utilities.addItemByMaterial(sack, String.valueOf(e.getItem().getItemStack().getType()), e.getItem().getItemStack().getAmount());
-            player.sendMessage("1" + e.getItem().getItemStack().getType() + e.getItem().getItemStack().getAmount());
-            e.setCancelled(true);
-            e.getItem().remove();
+        if(Utilities.findStringInList(Configuration.GetSackPickupMaterials(sackType), String.valueOf(e.getItem().getItemStack().getType()))) {
+            if((currentAmount + e.getItem().getItemStack().getAmount()) <= maxAmount) {
+                Utilities.addItemByMaterial(sack, String.valueOf(e.getItem().getItemStack().getType()), e.getItem().getItemStack().getAmount());
+                player.sendMessage("1" + e.getItem().getItemStack().getType() + e.getItem().getItemStack().getAmount());
+                e.setCancelled(true);
+                e.getItem().remove();
+            }
+            else {
+                e.setCancelled(true);
+                Integer toSack = maxAmount - currentAmount;
+                Integer toInv = e.getItem().getItemStack().getAmount() - toSack;
+                Utilities.addItemByMaterial(sack, String.valueOf(e.getItem().getItemStack().getType()), toSack);
+                ItemStack onGround = e.getItem().getItemStack();
+                onGround.setAmount(toInv);
+                e.getItem().setItemStack(onGround);
+                e.setCancelled(false);
+            }
         }
     }
 }
